3fbeb767cb22d051ee370858010bc7c187140c79
srouchier <s.rouchier@gmail.com>
2025-04-29 10:41:41 +0200

    initialised the documentation template

--- app.py
+++ app.py
@@ -1,23 +1,25 @@
-import dash
-import dash_core_components as dcc
-import dash_html_components as html
-from dash.dependencies import Input, Output
+from dash import Dash, html, dcc, callback, Output, Input
+import plotly.express as px
+import pandas as pd
 
-app = dash.Dash(__name__)
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv')
 
-app.layout = html.Div([
-    dcc.Input(id='input-box', type='text', value='Enter a value'),
-    html.Button('Submit', id='submit-button', n_clicks=0),
-    html.Div(id='output-container')
-])
+app = Dash()
 
-@app.callback(
-    Output('output-container', 'children'),
-    [Input('submit-button', 'n_clicks')],
-    [State('input-box', 'value')]
+# Requires Dash 2.17.0 or later
+app.layout = [
+    html.H1(children='Title of Dash App', style={'textAlign':'center'}),
+    dcc.Dropdown(df.country.unique(), 'Canada', id='dropdown-selection'),
+    dcc.Graph(id='graph-content')
+]
+
+@callback(
+    Output('graph-content', 'figure'),
+    Input('dropdown-selection', 'value')
 )
-def update_output(n_clicks, value):
-    return f'You entered {value} and clicked {n_clicks} times.'
+def update_graph(value):
+    dff = df[df.country==value]
+    return px.line(dff, x='year', y='pop')
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run(debug=True)
